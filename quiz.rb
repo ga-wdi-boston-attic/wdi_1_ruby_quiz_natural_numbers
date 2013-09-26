0d41a5da04d2526d2202c19c44831d9317a86b26
Matt Clement <darkmoves@gmail.com>
2013-09-26 10:27:18 -0400

    New update.

--- quiz.rb
+++ quiz.rb
@@ -2,15 +2,12 @@ require 'pry'
 # Return an array of all natural numbers below max_num that are divisible by multiple.
 
 # Defining the method.
-def multiple(multiple, max_num)
+def multiples(multiple, max_num)
 	naturals = []
-	each 
-	while n = 1...max_num
+	[1...max_num].each do |n|
 		if n % multiple = 0
 			naturals << n
 		end
 	end
 end
-
-puts multiple(10, 100)
 
