17f811fe20c7af152b2f46d61cac5603504ca09c
Amal Hussein <amalhussein08@gmail.com>
2013-09-26 10:31:51 -0400

    Final Quiz + Dave's Example

--- quiz.rb
+++ quiz.rb
@@ -7,6 +7,16 @@ def multiples (multiple, max_num)
 	 end
 	 results
 end 
+
 #puts multiples(3, 10)
 
+#--------------------DAVE's IN CLASS EXAMPLE------------------
 
+def multiples (multiple, max_num)
+	multiple_array = [ ]
+ # there are multiple ways of doing this 
+ 	multiple.upto(max_num - 1) do |value|
+ 	multiple_array << value if (value % multiple) == 0 
+end
+	return multiple_array
+end
