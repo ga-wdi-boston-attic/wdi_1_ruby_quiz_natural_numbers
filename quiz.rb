d0f8623ddb186ee58c6619f3eb46d33ce46a8e72
Ryan Page <rpage326@gmail.com>
2013-09-26 10:07:06 -0400

    Ryan Page QUIZ

---
+++ quiz.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+puts "Enter a max number: "
+gets.chomp
+puts "Enter"
+
+multiple = [x]
+
+def multiples(multiple, max_num)
+	x = max_num/multiple
+	x.each do |multiple|
+end
+
+
+while x<= max_num
+
+multiples(4,13)
+
+binding.pry
