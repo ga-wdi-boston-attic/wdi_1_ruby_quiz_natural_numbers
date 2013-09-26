ab6c38e7d6de377bb1a52752b4e7d3a71d48dd28
Bobby Mazzotti <robert.mazzoti@gmail.com>
2013-09-26 10:07:26 -0400

    wtf..mate?

---
+++ quiz.rb
@@ -0,0 +1,20 @@
+#Quiz Week 01
+
+### To Do
+# - Write a function called `multiples` that takes parameters called `multiple` and `max_num`.
+# - The function should *return* an *array* of all the [natural numbers](http://en.wikipedia.org/wiki/Natural_number) below `max_num` that are multiples of `multiple`.
+
+# ## Tips
+# - Run `bundle install` in this directory after you clone this repository
+# - Run `rspec spec` to check your results
+# - Make sure the names of your method and parameters match the assignment
+# - Turn in this quiz via a pull request on Github to the instructor.
+
+
+def multiples(multiple, max_num)
+	natural_numbers = []
+	(multiple..max_num).each do |i|
+		natural_numbers << i if	max_num % mutiple == 0 
+	end
+	return natural_numbers
+end
