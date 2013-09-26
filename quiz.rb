99789061572131d4cca2c053cf0d278e80ed27a2
Ben Winter <ben.g.winter@gmail.com>
2013-09-26 09:42:02 -0400

    working product

---
+++ quiz.rb
@@ -0,0 +1,11 @@
+def multiples(multiple, max_num)
+	counter = 1
+	my_array = []
+	while (multiple * counter) <= max_num
+		my_array << (multiple * counter)
+		counter = counter + 1
+	end
+	puts my_array
+end
+
+multiples(9, 200)
