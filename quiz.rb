55484cdd5d1aefd754df47684f65f4f7577ff141
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-28 08:49:02 -0400

    I have edited this program. I realize that defining an array to receive input from the loop has been tripping me up. I also think that I need more practice with conditional and iteration syntax

--- quiz.rb
+++ quiz.rb
@@ -1,13 +1,31 @@
-require "pry"
+# def multiples(multiple, max_num) 
+# 	multiples = []
+# 	 (0..max_num).each do |n|
 
-def multiples(multiple, max_num) 
-	multiples = []
-	 (0..max_num).each do |n|
-
-		multiples.push(multiple*n)
+# 		multiples.pushquit(multiple*n)
 	 	
+# 	end
+	
+	
+# end
+# multiples(2,5)
+
+#class example
+# def multiples(multiple, max_num) 
+# 	multiple_array = []
+# 	multiple.upto(max_num) do |value|
+# 		multiple_array << value if (value % multiple) == 0
+# 	end
+# 	return multiple_array
+# end
+
+
+require 'pry'
+def multiple_spitter(multiple, max_number)
+	multiple_array = [] 
+	multiple.upto(max_number) do |x|                                    
+	multiple_array << x if (x % multiple) == 0
 	end
-	
-	
+		return multiple_array
 end
-multiples(2,5)
+binding.pry
