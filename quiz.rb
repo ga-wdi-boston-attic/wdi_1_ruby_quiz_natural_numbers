6da649ed1e8b11a46f43482555241f3328782de5
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-09 09:26:19 -0500

    I gave it a shot ... not able to think...

---
+++ quiz.rb
@@ -0,0 +1,13 @@
+#Write a function called multiples that takes parameters called multiple and max_num.
+
+#The function should return an array of all the natural numbers below max_num that are multiples of multiple.
+
+def multiples(multiple, max_num)
+	while i <= max_num
+		return multiple * i
+	end
+end
+
+multi = []
+
+
