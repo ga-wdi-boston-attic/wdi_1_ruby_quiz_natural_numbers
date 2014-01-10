2346eac8a9bbb7f5fe117384090fc283efa0526f
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-10 17:47:13 -0500

    done

--- quiz.rb
+++ quiz.rb
@@ -1,12 +1,10 @@
 def multiples(multiple, max_num)
+
 	multiple_array = []
-	while multiple <= max_num
-		if max_num % multiple == 0
-			multiple_array.unshift(multiple)
-			multiple += 1
-		end
+	current_num = multiple
+	while current_num < max_num
+		multiple_array << current_num
+		current_num += multiple
 	end
 	return multiple_array
 end
-
-puts multiples(3, 8)
